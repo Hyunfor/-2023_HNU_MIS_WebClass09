c6f586229c7a882a0d20ada28893e578837cf6b7
Hyunfor <navel5148@gmail.com>
2023-05-09 09:41:10 +0900

    Item등록 과제 준비 및 jsp, from, servlet 생성

---
+++ WebContent/allItem.jsp
@@ -0,0 +1,69 @@
+<%@page import="java.sql.DriverManager"%>
+<%@page import="java.sql.ResultSet"%>
+<%@page import="java.sql.Statement"%>
+<%@page import="java.sql.Connection"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<%! // 선언부는 첫 방문자에 의해서 단 한번만 수행
+	Connection conn = null;
+	Statement stmt = null;
+	ResultSet rs = null;
+	
+	String url = "jdbc:oracle:thin:@localhost:1521:XE";
+	String uid = "mis";
+	String pass = "1234";
+	String sql = "SELECT * FROM ITEM";
+%>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>JDBC</title>
+</head>
+<body>
+	 <table width="800" border="1">
+	 	<tr>
+	 		<th>이름</th>
+	 		<th>아이디</th>
+	 		<th>암호</th>
+	 		<th>이메일</th>
+	 		<th>전화번호</th>
+	 		<th>권한(1:관리자, 2:일반유저)</th>
+	 	</tr>
+	 	
+	 	<%
+	 		try{
+	 			//1. 드라이버 로딩
+	 			Class.forName("oracle.jdbc.driver.OracleDriver");
+	 			
+	 			//2. Connection 객체 생성
+	 			conn = DriverManager.getConnection(url, uid, pass);
+	 			
+	 			//3. Statement 객체 얻어오기
+	 			stmt = conn.createStatement();
+	 			
+	 			//4. 쿼리 실행 후 ResultSet 객체 얻어오기
+	 			rs = stmt.executeQuery(sql);
+	 			
+	 			//5. 결과 출력
+	 			while(rs.next()){
+	 				out.print("<tr>");
+	 				
+	 				out.print("<td>" + rs.getString("name") + "</td>");
+	 				out.print("<td>" + rs.getString("userid") + "</td>");
+	 				out.print("<td>" + rs.getString("pwd") + "</td>");
+	 				out.print("<td>" + rs.getString("email") + "</td>");
+	 				out.print("<td>" + rs.getString("phone") + "</td>");
+	 				out.print("<td>" + rs.getString("admin") + "</td>");
+	 				
+	 				out.print("</tr>");
+	 			}
+	 			
+	 		}catch (Exception e){
+	 			e.printStackTrace();
+	 		}
+	 	%>
+	 	
+	 </table>
+</body>
+</html>
---
+++ src/com/mis/controller/ItemServlet.java
@@ -0,0 +1,102 @@
+package com.mis.controller;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MemberServlet
+ */
+@WebServlet("/MemberServlet")
+public class ItemServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ItemServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		response.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html; charset=UTF-8");
+		request.setCharacterEncoding("UTF-8");
+		
+		// 아이템 등록 정보 가져오기
+		String name = request.getParameter("name");
+		String price = request.getParameter("price");
+		String elucidate = request.getParameter("elucidate");
+		
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		String url = "jdbc:oracle:thin:@localhost:1521:XE";
+		String uid = "mis";
+		String pass = "1234";
+		String sql = "INSERT INTO Item VALUES(?, ?, ?, ?, ?, ?)";
+		
+		try{
+			
+			//1. 드라이버 로딩
+ 			Class.forName("oracle.jdbc.driver.OracleDriver");
+ 			
+ 			//2. Connection 객체 생성
+ 			conn = DriverManager.getConnection(url, uid, pass);
+ 			
+ 			//3. PreparedStatement 객체 얻어오기
+ 			pstmt = conn.prepareStatement(sql);
+ 			
+ 			//4. 값 채우기
+ 			pstmt.setString(1, name);
+ 			pstmt.setString(2, price);
+ 			pstmt.setString(3, elucidate);
+ 			
+ 			//5. 쿼리 실행
+ 			pstmt.executeUpdate();
+			
+		}catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			// 리소스 반환
+			try {
+				if(pstmt != null){
+					pstmt.close();
+				}
+				if(conn != null){
+					conn.close();
+				}
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}
+		
+		// 페이지 이동
+		RequestDispatcher dispatcher = request.getRequestDispatcher("allItem.jsp");
+		dispatcher.forward(request, response);
+	}
+
+}
